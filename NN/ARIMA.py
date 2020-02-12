25bcd3dbada07053522dac3afad2fa54655f13b3
aattwwss <44013904+aattwwss@users.noreply.github.com>
2020-02-12 12:57:16 +0800

    bn

--- NN/ARIMA.py
+++ NN/ARIMA.py
@@ -3,19 +3,30 @@ from pandas import datetime
 from pandas import DataFrame
 from statsmodels.tsa.arima_model import ARIMA
 from matplotlib import pyplot
+from sklearn.metrics import mean_squared_error
 
 def parser(x):
 	return datetime.strptime('190'+x, '%Y-%m')
 
-series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-# fit model
-model = ARIMA(series, order=(5,1,0))
-model_fit = model.fit(disp=0)
-print(model_fit.summary())
-# plot residual errors
-residuals = DataFrame(model_fit.resid)
-residuals.plot()
-pyplot.show()
-residuals.plot(kind='kde')
-pyplot.show()
-print(residuals.describe())
+#series = read_csv('data/shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+series = read_csv('data/1000.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+X = series.values
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size:len(X)]
+history = [x for x in train]
+predictions = list()
+for t in range(len(test)):
+	model = ARIMA(history, order=(7,1,0))
+	model_fit = model.fit(disp=0)
+	output = model_fit.forecast()
+	yhat = output[0]
+	predictions.append(yhat)
+	obs = test[t]
+	history.append(obs)
+	print('predicted=%f, expected=%f' % (yhat, obs))
+error = mean_squared_error(test, predictions)
+print('Test MSE: %.3f' % error)
+# plot
+pyplot.plot(test)
+pyplot.plot(predictions, color='red')
+pyplot.savefig("123.png")
